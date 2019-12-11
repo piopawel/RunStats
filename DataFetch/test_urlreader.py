66c1f6108d41ba912c6c45f92ad4be61ffbb1c3b
Piotr Pawelczyk <piopawelczyk@gmail.com>
2019-12-11 14:11:37 +0100

    DataFetch/urlreader: changed the structure to more separate functions, updated the test

--- DataFetch/test_urlreader.py
+++ DataFetch/test_urlreader.py
@@ -1,25 +1,34 @@
 import unittest
+from unittest.mock import patch
+
 import urlreader
 import random
 import urllib.error as url_error
 
 class TestUrlReader(unittest.TestCase):
 
-    def test_fetch_HTML(self):
-        result, response = urlreader.fetch_HTML("https://www.parkrun.pl/gdynia/rezultaty/weeklyresults/?runSeqNumber=408")
-        self.assertEqual(response, 200)
-        # result, response = urlreader.fetch_HTML("https://www.parkrun.pl/dummy_url")
-        # self.assertEqual(response, 404)
+    def test_fetch_response(self):
+        response = urlreader.fetch_response("https://www.parkrun.pl/gdynia/rezultaty/weeklyresults/?runSeqNumber=408")
+        self.assertEqual(response.code, 200)
+
         # test a random page number
         random_number = random.randint(0, 400)
-        result, response = urlreader.fetch_HTML(f'https://www.parkrun.pl/gdynia/rezultaty/weeklyresults/?runSeqNumber={random_number}')
-        self.assertEqual(response, 200)
+        response = urlreader.fetch_response(f'https://www.parkrun.pl/gdynia/rezultaty/weeklyresults/?runSeqNumber={random_number}')
+        self.assertEqual(response.code, 200)
+
+    def test__get_HTML_text(self):
+
+        # result, response = urlreader.fetch_HTML("https://www.parkrun.pl/dummy_url")
+        # self.assertEqual(response, 404)
 
         with self.assertRaises(url_error.HTTPError):
-            urlreader.fetch_HTML("https://www.parkrun.pl/dummy_url")
+            response = urlreader.fetch_response("https://www.parkrun.pl/dummy_url")
+            urlreader._get_HTML_text(response)
 
         with self.assertRaises(ValueError):
-            urlreader.fetch_HTML("random_text")
+            response = urlreader.fetch_response("random_text")
+            urlreader._get_HTML_text(response)
+
 
 if __name__ == '__main__':
     unittest.main()
--- DataFetch/urlreader.py
+++ DataFetch/urlreader.py
@@ -1,13 +1,18 @@
 import urllib.request as url_request
 import urllib.error as url_error
 
+def get_HTML(url, data=None, username=None, password=None):
+    response = fetch_response(url, data ,username, password)
+    return _get_HTML_text(response)
 
-def fetch_HTML(url, data=None, username=None, password=None):
+def fetch_response(url, data=None, username=None, password=None):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'}
     request = url_request.Request(url, data=None, headers=headers)
+    return url_request.urlopen(request)
+
+def _get_HTML_text(response):
     try:
-        response = url_request.urlopen(request)
         charset = response.info().get_content_charset()
         htmlBytes = response.read()
         if charset is not None:
@@ -15,7 +20,7 @@ def fetch_HTML(url, data=None, username=None, password=None):
         else:
             htmlStr = htmlBytes.decode('utf-8')
         return htmlStr
-    # except url_error.HTTPError as e:
-    #     raise e
+        # except url_error.HTTPError as e:
+        #     raise e
     except Exception as e:
         raise e
