80de78d842179d4fc094baccc13b78a5653b5f9d
Piotr Pawelczyk <piopawelczyk@gmail.com>
2019-12-12 13:52:28 +0100

    WebaPP: added Profile model

--- WebApp/users/models.py
+++ WebApp/users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    name = models.CharField(max_length=100)
+    gender = models.CharField(max_length=1)
+    club = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
