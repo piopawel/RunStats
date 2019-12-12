2d9a9f1713d429f1734fb7a7601fd65215248ce2
Piotr Pawelczyk <piopawelczyk@gmail.com>
2019-12-12 15:50:12 +0100

    WebApp: migrations for Result, Evemt, Profile

--- WebApp/base/models.py
+++ WebApp/base/models.py
@@ -1,6 +1,8 @@
 from django.db import models
-from ..run_statistics.models import Event
-from ..users.models import Profile
+from run_statistics.models import Event
+from users.models import Profile
+# I run the app from "WebApp level, Python discards the knowledge of where this exact script is
+# So i cannot use ../run_statistics.models import Event, instead I run it without ".." as seen from the WebApp dir
 
 class Result(models.Model):
     event = models.ForeignKey(Event, on_delete=models.DO_NOTHING)
