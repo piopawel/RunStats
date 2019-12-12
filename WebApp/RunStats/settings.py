b119acf44256f72cda462720371be8c13857d81f
Piotr Pawelczyk <piopawelczyk@gmail.com>
2019-12-12 13:41:26 +0100

    WebApp: added users application

--- WebApp/RunStats/settings.py
+++ WebApp/RunStats/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'base.apps.BaseConfig',
+    'run_statistics.apps.RunStatisticsConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
