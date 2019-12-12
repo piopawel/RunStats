0b8ba04526b49b154735c425520ee4f7fa81d40d
Piotr Pawelczyk <piopawelczyk@gmail.com>
2019-12-12 13:59:00 +0100

    WebApp: added Result model

--- WebApp/base/models.py
+++ WebApp/base/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from ..run_statistics.models import Event
+from ..users.models import Profile
 
-# Create your models here.
+class Result(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.DO_NOTHING)
+    person = models.ForeignKey(Profile, on_delete=models.DO_NOTHING)
+    finished = models.IntegerField()
+    time = models.TimeField()
+    age_group = models.CharField(max_length=10)
+    age_percentage = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.person, self.event
+
